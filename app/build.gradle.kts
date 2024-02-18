ea8f388014bce8d61c6d5b25bf88b0e658496ef0
Obydennov Dmitrii <dima-obydennov@mail.ru>
2024-02-18 21:46:37 +0300

    new gradle and 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     java
+    checkstyle
+    jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    checkstyle
 }
 
 group = "hexlet.code"
