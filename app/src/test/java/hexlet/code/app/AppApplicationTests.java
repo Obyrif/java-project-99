58842f1d67ad62f00b517ebf493d18659b5a3338
Obydennov Dmitrii <dima-obydennov@mail.ru>
2024-02-22 00:21:40 +0300

    add fix

--- app/src/test/java/hexlet/code/app/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,7 +17,6 @@ class AppApplicationTests {
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser(username = "user", password = "password", roles = "USER")
     void testWelcomePage() throws Exception {
         var result = mockMvc.perform(get("/welcome"))
                 .andExpect(status().isOk())
